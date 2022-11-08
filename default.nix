df6aca25aad0df783619917082618529155818b6
Jack Baldry <jack.baldry@grafana.com>
2022-11-08 11:59:33 -0400

    Add nixosModules for systemd service
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- default.nix
+++ default.nix
@@ -8,6 +8,12 @@ buildGoModule rec {
   version = "0.1.0";
   vendorSha256 = "sha256-hyn8oAs2GaH2p6awQ3mYMDYhaxsx8cKn9SmCbKpm6FM=";
 
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+  postInstall = ''
+    wrapProgram $out/bin/xinput_exporter --set PATH "${pkgs.xorg.xinput}/bin"
+  '';
+
+
   meta = with lib; {
     description = "Expose xevents from xinput as Prometheus metrics.";
     homepage = "https://github.com/jdbaldry/xinput_exporter";
--- flake.nix
+++ flake.nix
@@ -6,12 +6,60 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     {
-      overlay = import ./overlay.nix;
-    } //
-    (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-      in {
-        devShell = import ./shell.nix { inherit pkgs; };
-        defaultPackage = pkgs.xinput_exporter;
-      }));
+      overlay = final: prev: {
+        xinput_exporter = prev.callPackage ./default.nix { pkgs = prev.pkgs; };
+      };
+      nixosModules = rec {
+        default = xinput_exporter;
+        xinput_exporter = { config, lib, pkgs, ... }:
+          with lib;
+          let
+            cfg = config.services.xinput_exporter;
+            drv = pkgs.callPackage ./default.nix { inherit pkgs; };
+          in
+          {
+            options.services.xinput_exporter = {
+              enable = mkEnableOption "xinput_exporter";
+              display = mkOption {
+                description = "The value for the DISPLAY environment variable.";
+                default = ":0";
+                type = types.str;
+              };
+
+              listen-address = mkOption {
+                description = "The address to listen on for HTTP requests.";
+                default = ":9191";
+                type = types.str;
+              };
+
+              user = mkOption {
+                description = "The user to use for their .Xauthority cookie.";
+                type = types.str;
+              };
+            };
+
+            config = mkIf cfg.enable {
+              systemd.services.xinput_exporter = {
+                description = "Exporter of xevents using xinput";
+
+                serviceConfig = {
+                  Environment = [ "DISPLAY=${cfg.display}" ];
+                  ExecStart = "${drv}/bin/xinput_exporter -listen-adress=${cfg.listen-address}";
+                  Restart = "always";
+                  User = cfg.user;
+                };
+
+                wantedBy = [ "graphical.target" ];
+              };
+            };
+          };
+      } //
+      (flake-utils.lib.eachSystem [ "x86_64-linux" ]
+        (system:
+          let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+          in {
+            defaultPackage = pkgs.xinput_exporter;
+            devShell = import ./shell.nix { inherit pkgs; };
+          }));
+    };
 }
